9e3cc28dbce16de23c6ffbd615ee836e1a21d939
Marcin Pas <marcin.pas@softelnet.com>
2019-05-29 14:32:08 +0200

    Support for events.

--- lib/src/event.dart
+++ lib/src/event.dart
@@ -1,4 +1,4 @@
-// Copyright 2018 The Sponge authors.
+// Copyright 2019 The Sponge authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,20 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export 'src/constants.dart';
-export 'src/context.dart';
-export 'src/event.dart';
-export 'src/exception.dart';
-export 'src/listener.dart';
-export 'src/meta.dart';
-export 'src/request.dart';
-export 'src/response.dart';
-export 'src/rest_client.dart';
-export 'src/rest_client_configuration.dart';
-export 'src/type.dart';
-export 'src/type_converter.dart';
-export 'src/type_value.dart';
-export 'src/util/sub_action.dart';
-export 'src/util/type_utils.dart';
-export 'src/util/validate.dart';
-export 'src/utils.dart';
+import 'package:meta/meta.dart';
+
+/// A Sponge event.
+class SpongeEvent {
+  SpongeEvent({
+    @required this.id,
+    @required this.name,
+    @required this.time,
+    @required this.priority,
+    Map<String, dynamic> attributes,
+  }) : this.attributes = attributes ?? {};
+
+  String id;
+  String name;
+  DateTime time;
+  int priority;
+  Map<String, dynamic> attributes;
+}

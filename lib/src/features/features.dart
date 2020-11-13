85f7679aeb7a19cba541d1928b5f29febeb678ba
Marcin Pas <marcin.pas@softelnet.com>
2020-11-13 17:39:11 +0100

    Support for StringType URL.

--- lib/src/features/features.dart
+++ lib/src/features/features.dart
@@ -42,6 +42,7 @@ class Features {
   static const TYPE_CHARACTERISTIC_DRAWING = 'drawing';
   static const TYPE_CHARACTERISTIC_COLOR = 'color';
   static const TYPE_CHARACTERISTIC_NETWORK_IMAGE = 'networkImage';
+  static const TYPE_CHARACTERISTIC_URL = 'url';
 
   static const TYPE_FILENAME = 'filename';
 
